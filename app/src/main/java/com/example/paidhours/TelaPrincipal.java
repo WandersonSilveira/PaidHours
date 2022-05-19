5bd37ec70b8eeea8cbc551f870c3297c9271dfec
WandersonSilveira <wanderson.silveira23@gmail.com>
2022-05-19 07:30:34 -0300

    Inicio projeto

---
+++ app/src/main/java/com/example/paidhours/TelaPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.paidhours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal);
+    }
+}
