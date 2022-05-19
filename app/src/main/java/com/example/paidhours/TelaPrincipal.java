9c1d42029693b67705aa81e24ef24b2754f32ba5
WandersonSilveira <wanderson.silveira23@gmail.com>
2022-05-19 08:01:55 -0300

    Splash screen

--- app/src/main/java/com/example/paidhours/TelaPrincipal.java
+++ app/src/main/java/com/example/paidhours/TelaPrincipal.java
@@ -9,6 +9,16 @@ public class TelaPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        proTiraSplashScreen();
         setContentView(R.layout.tela_principal);
+    }
+
+    private void proTiraSplashScreen(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_PaidHours);
     }
 }
